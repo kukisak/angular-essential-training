3698eef2d88c3550c54be33d46e53029687f0910
kukralp <kukral.petr@hypotecnibanka.cz>
2023-07-15 16:43:26 +0200

    applied changes from video

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  template: '<h1>My App</h1>'
+  selector: 'mw-app',
+  template: `
+    <h1>My App</h1><p>Keeping track of the media I want to watch still</p>
+    `
 })
 export class AppComponent {}
